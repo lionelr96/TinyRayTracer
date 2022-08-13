44b8099831a0fd362c32aa88339c31744724a27a
LIONEL ROBERT <lionelr@umich.edu>
2022-08-13 19:49:31 -0400

    file cleanup 3

--- TinyRayTracer/main.cpp
+++ TinyRayTracer/main.cpp
@@ -5,12 +5,6 @@
 #include <vector>
 #include "geometry.h"
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
-
 void render () {
 	const int width = 1024;
 	const int height = 768;
